b14fe855ca928abd75fa48ad1b360167c4767787
ruaanls <ruanlima257@hotmail.com>
2025-03-18 22:51:14 -0300

    JPA

--- apiRest/src/main/java/br/com/fiap/apiRest/ApiRestApplication.java
+++ apiRest/src/main/java/br/com/fiap/apiRest/ApiRestApplication.java
@@ -1,9 +1,12 @@
 package br.com.fiap.apiRest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API de livros", description = "Exemplo de API Restful com swagger da 2tdspw", version = "v1"))
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
--- apiRest/src/main/java/br/com/fiap/apiRest/controller/LivroController.java
+++ apiRest/src/main/java/br/com/fiap/apiRest/controller/LivroController.java
@@ -5,6 +5,12 @@ import br.com.fiap.apiRest.DTO.LivroResponse;
 import br.com.fiap.apiRest.Service.LivroService;
 import br.com.fiap.apiRest.model.Livro;
 import br.com.fiap.apiRest.repository.LivroRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -23,7 +29,8 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping(value = "/livros")
+@RequestMapping(value = "/livros", consumes = {"application/json"}) // Defina que o tipo da api é json
+@Tag(name = "api-livros") // Nome da api será api livros
 public class LivroController
 {
     @Autowired
@@ -31,6 +38,14 @@ public class LivroController
     @Autowired
     private LivroService service;
 
+    @Operation(summary = "Criação de um novo livro")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Livro criado com sucesso",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Livro.class)) // Classe que deverá ser base para a response
+            }),
+            @ApiResponse(responseCode = "400", description = "Informações inválidas ou nulas", content = @Content(schema = @Schema()))
+    })
     @PostMapping
     public ResponseEntity<Livro> createLivro(@RequestBody @Valid LivroRequest livro){
         LivroService serviceLivro = new LivroService();
@@ -39,6 +54,14 @@ public class LivroController
         return new ResponseEntity<>(livroSalvo, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Lista todos os livros por paginação (COM PAGE NUMBER)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Livros listados com sucesso",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Livro.class)) // Classe que deverá ser base para a response
+                    }),
+            @ApiResponse(responseCode = "400", description = "Informações inválidas ou nulas", content = @Content(schema = @Schema()))
+    })
     @GetMapping
     public ResponseEntity<Page<LivroResponse>> getLivros(@RequestParam(defaultValue = "0") Integer pageNumber)
     {
@@ -58,6 +81,14 @@ public class LivroController
     }
 
 
+    @Operation(summary = "Lista todos os livros de uma página especifica (LISTAGEM PRINCIPAL)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Livro criado com sucesso",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Livro.class)) // Classe que deverá ser base para a response
+                    }),
+            @ApiResponse(responseCode = "400", description = "Informações inválidas ou nulas", content = @Content(schema = @Schema()))
+    })
     @GetMapping("/page/{id}")
     public ResponseEntity<Page<LivroResponse>> getLivros2(@PathVariable int id)
     {
@@ -77,6 +108,14 @@ public class LivroController
         return new ResponseEntity<>(service.findall(pageable), HttpStatus.OK);
     }
 
+    @Operation(summary = "Consultar um livro específico por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Livro criado com sucesso",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Livro.class)) // Classe que deverá ser base para a response
+                    }),
+            @ApiResponse(responseCode = "400", description = "Informações inválidas ou nulas", content = @Content(schema = @Schema()))
+    })
     @GetMapping("/{id}")
 
     public ResponseEntity<LivroResponse> livroByID(@PathVariable Long id)
@@ -98,6 +137,14 @@ public class LivroController
     }
 
 
+    @Operation(summary = "Editar algum livro existente através do id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Livro criado com sucesso",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Livro.class)) // Classe que deverá ser base para a response
+                    }),
+            @ApiResponse(responseCode = "400", description = "Informações inválidas ou nulas", content = @Content(schema = @Schema()))
+    })
     @PutMapping ("/{id}")
     public ResponseEntity<Livro> updateLivro(@RequestBody LivroRequest livroNovo, @PathVariable Long id)
     {
@@ -113,9 +160,22 @@ public class LivroController
         return new ResponseEntity<>(livroSalvo, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Exclui um livro através do ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Livro criado com sucesso",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Livro.class)) // Classe que deverá ser base para a response
+                    }),
+            @ApiResponse(responseCode = "400", description = "Informações inválidas ou nulas", content = @Content(schema = @Schema()))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteLivro(@PathVariable long id)
     {
+        Optional<Livro> livroVelho = livroRepository.findById(id);
+        if(livroVelho.isEmpty())
+        {
+            return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+        }
         livroRepository.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
